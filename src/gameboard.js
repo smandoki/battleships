20c913f7fa3cef0d7bba755ea1a9ec212ed8e41c
smandoki <80867148+smandoki@users.noreply.github.com>
2022-11-04 18:16:56 +0800

    Added tests for gameboard class
    modified gameboard implementation and have passed all of the added
    tests

--- src/gameboard.js
+++ src/gameboard.js
@@ -4,35 +4,55 @@ import { create2DArray, deepCloneArray } from './util';
 const GRID_SIZE = 7;
 
 export default class Gameboard {
+    #ships = {};
+    #board;
+    #autoIncId = 0;
+
     constructor() {
         this.#board = create2DArray(GRID_SIZE, GRID_SIZE);
-        this.#ships = [];
     }
 
+    /**
+     * Adds a new ship specified location
+     * 
+     * @param {number} x x-coordinate to place ship
+     * @param {number} y y-coordinate to place ship
+     * @param {number} length length of the ship to be placed
+     * @param {string} direction direction of ship either 'row' or 'col'
+     * @returns void
+     */
     addShip(x, y, length, direction) {
         const ship = new Ship(length);
 
         for (let i = 0; i < length; i++) {
             if (direction === 'row') {
-                this.#board[x + i][y] = ship;
+                this.#board[x + i][y] = this.#autoIncId;
             } else {
-                this.#board[x][y + i] = ship;
+                this.#board[x][y + i] = this.#autoIncId;
             }
         }
 
-        this.#ships.push(ship);
+        this.#ships[this.#autoIncId++] = ship;
     }
 
     receiveAttack(x, y) {
         if (this.#board[x][y] === undefined) {
             this.#board[x][y] = 'miss';
         } else {
-            this.#board[x][y].hit();
+            this.#ships[this.#board[x][y]].hit();
             this.#board[x][y] = 'hit';
         }
     }
 
-    //check if ship can be placed here
+    /**
+     * Determines if a ship can be placed here
+     * 
+     * @param {number} x x-coordinate to place ship
+     * @param {number} y y-coordinate to place ship
+     * @param {number} length length of the ship to be placed
+     * @param {string} direction direction of ship either 'row' or 'col'
+     * @returns void
+     */
     isValidPlacement(x, y, length, direction) {
         for (let i = 0; i < length; i++) {
             if (direction === 'row') {
@@ -54,9 +74,11 @@ export default class Gameboard {
     }
 
     isAllShipsSunk() {
-        this.#ships.forEach(ship => {
-            if (!ship.isSunk()) return false;
-        });
+        if (Object.keys(this.#ships).length === 0) return false;
+
+        for (const [key, value] of Object.entries(this.#ships)) {
+            if (!value.isSunk()) return false;
+        }
 
         return true;
     }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,2 +1,72 @@
 import Gameboard from "../gameboard";
 
+test('test add ship function', () => {
+    const gameboard = new Gameboard();
+    gameboard.addShip(0, 0, 1, 'row');
+
+    expect(gameboard.getBoard()).toEqual([
+        [0], [], [], [], [], [], []
+    ]);
+});
+
+test('test add ship function with two ships', () => {
+    const gameboard = new Gameboard();
+    gameboard.addShip(0, 0, 1, 'row');
+    gameboard.addShip(1, 0, 2, 'col');
+
+    expect(gameboard.getBoard()).toEqual([
+        [0], [1, 1], [], [], [], [], []
+    ]);
+});
+
+test('test all ships sunk is false with no ships', () => {
+    const gameboard = new Gameboard();
+
+    expect(gameboard.isAllShipsSunk()).toBeFalsy();
+});
+
+test('test all ships sunk is false with ships', () => {
+    const gameboard = new Gameboard();
+    gameboard.addShip(0, 0, 1, 'row');
+
+    expect(gameboard.isAllShipsSunk()).toBeFalsy();
+});
+
+test('test receiveAttack hit and all ships sunk is true', () => {
+    const gameboard = new Gameboard();
+    gameboard.addShip(0, 0, 1, 'row');
+    gameboard.receiveAttack(0, 0);
+
+    expect(gameboard.isAllShipsSunk()).toBeTruthy();
+    expect(gameboard.getBoard()).toEqual([
+        ['hit'], [], [], [], [], [], []
+    ]);
+});
+
+test('test receiveAttack missed attack', () => {
+    const gameboard = new Gameboard();
+    gameboard.receiveAttack(1, 0);
+
+    expect(gameboard.getBoard()).toEqual([
+        [], ['miss'], [], [], [], [], []
+    ]);
+});
+
+test('test isValidPlacement is true', () => {
+    const gameboard = new Gameboard();
+    
+    expect(gameboard.isValidPlacement(0, 0, 1, 'row')).toBeTruthy();
+});
+
+test('test isValidPlacement is false at border', () => {
+    const gameboard = new Gameboard();
+    
+    expect(gameboard.isValidPlacement(7, 0, 2, 'row')).toBeFalsy();
+});
+
+test('test isValidPlacement is false from ship overlap', () => {
+    const gameboard = new Gameboard();
+    gameboard.addShip(1, 0, 1, 'row');
+    
+    expect(gameboard.isValidPlacement(0, 0, 2, 'row')).toBeFalsy();
+});
